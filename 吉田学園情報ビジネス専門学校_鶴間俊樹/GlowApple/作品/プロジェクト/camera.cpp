63c8440d279d3e94daf3519d97895c6485789d9e
ToshikiTsuruma <201020@st.yoshida-g.jp.ac>
2022-08-30 17:36:11 +0900

    データの更新

---
+++ 吉田学園情報ビジネス専門学校_鶴間俊樹/GlowApple/作品/プロジェクト/camera.cpp
@@ -0,0 +1,291 @@
+//=============================================================================
+//
+// カメラ処理 [camera.cpp]
+// Author : 鶴間俊樹
+//
+//=============================================================================
+#include "camera.h"
+#include "manager.h"
+#include "renderer.h"
+#include "input.h"
+
+#include "scene.h"
+#include "player.h"
+
+//=============================================================================
+// マクロ定義
+//=============================================================================
+#define MAX_DRAW_DISTANCE (5000.0f)		//描画可能な最大の距離
+#define DEFAULT_CAMERA_DISTANCE (1500.0f)	//視点と注視点の距離
+#define CAMERA_ROTATE_SPEED_YAW (0.008f)	//カメラの回転速度(Y軸)
+#define CAMERA_ROTATE_SPEED_PITCH (0.006f)	//カメラの回転速度(X軸)
+#define MAX_ROTATE_PITCH (-0.05f)	//カメラのX軸回転の最大
+#define MIN_ROTATE_PITCH (-0.18f)	//カメラのX軸回転の最低
+
+//=============================================================================
+// デフォルトコンストラクタ
+//=============================================================================
+CCamera::CCamera()
+{
+	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_rot = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_posV = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_posR = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_fDist = 0.0f;
+	m_vecU = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	D3DXMatrixIdentity(&m_mtxProjection);
+	D3DXMatrixIdentity(&m_mtxView);
+	ZeroMemory(&m_viewport, sizeof(D3DVIEWPORT9));
+
+	m_bLockControll = false;
+}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CCamera::~CCamera()
+{
+
+}
+
+//=============================================================================
+//生成処理
+//=============================================================================
+CCamera* CCamera::Create() {
+	CCamera* pCamera;
+	pCamera = new CCamera;
+	if (pCamera == nullptr) return nullptr;
+
+	pCamera->Init();
+
+	return pCamera;
+}
+
+//=============================================================================
+// カメラの初期化処理
+//=============================================================================
+HRESULT CCamera::Init(void) {
+	m_pos = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_rot.x = D3DX_PI * -0.15f;
+	m_rot.y = D3DX_PI * 0.0f;
+
+	LPDIRECT3DDEVICE9 pDevice = nullptr;	//デバイスへのポインタ
+	//マネージャーの取得
+	CManager* pManager = CManager::GetManager();	
+	//レンダラーの取得
+	CRenderer* pRenderer = nullptr;			
+	if (pManager != nullptr) pRenderer = pManager->GetRenderer();
+	//デバイスの取得
+	if (pRenderer != nullptr) pDevice = pRenderer->GetDevice();
+	//デバイスがnullの場合終了
+	if (pDevice == nullptr) return S_OK;
+
+	//ビューポートの設定
+	m_viewport.X = (DWORD)0;
+	m_viewport.Y = (DWORD)0;
+	m_viewport.Width = (DWORD)SCREEN_WIDTH;
+	m_viewport.Height = (DWORD)SCREEN_HEIGHT;
+	m_viewport.MinZ = 0.0f;
+	m_viewport.MaxZ = 1.0f;
+	//ビューポートの設定
+	pDevice->SetViewport(&m_viewport);
+
+	//プロジェクションマトリックスを作成
+	D3DXMatrixPerspectiveFovLH(&m_mtxProjection,
+		D3DXToRadian(45.0f),						//画角 360度
+		(float)SCREEN_WIDTH / (float)SCREEN_HEIGHT,	//画面比率
+		10.0f,										//手前の位置
+		MAX_DRAW_DISTANCE);							//奥の位置
+
+	//プロジェクションマトリックスの設定
+	pRenderer->SetEffectMatrixProj(m_mtxProjection);
+
+	//上ベクトルの設定
+	m_vecU = D3DXVECTOR3(0.0f, 1.0f, 0.0f);
+
+	//ビューマトリックスの設定
+	SetCamera();
+
+	//距離の設定
+	SetDistance(DEFAULT_CAMERA_DISTANCE);
+
+	return S_OK;
+}
+
+//=============================================================================
+// カメラの終了処理
+//=============================================================================
+void CCamera::Uninit(void) {
+
+}
+
+//=============================================================================
+// カメラの更新処理
+//=============================================================================
+void CCamera::Update(void) {
+	//移動処理
+	//Move();
+
+	//回転処理
+	if(!m_bLockControll) Rotate();
+}
+
+//=============================================================================
+// カメラのセット
+//=============================================================================
+void CCamera::SetCamera(void) {
+	//マネージャーの取得
+	CManager* pManager = CManager::GetManager();
+	if (pManager == nullptr) return;
+	//レンダラーの取得
+	CRenderer* pRenderer = pManager->GetRenderer();
+	if (pRenderer == nullptr) return;
+	//デバイスの取得
+	LPDIRECT3DDEVICE9 pDevice = pRenderer->GetDevice();
+	if (pDevice == nullptr) return;
+
+
+	//------------------------------------
+	//プレイヤーとの位置の間を取得
+	//------------------------------------
+	CScene* pScene = pManager->GetScene();
+	CPlayer* pPlayer = nullptr;
+	if (pScene != nullptr) pPlayer = pScene->GetPlayer();
+
+	D3DXVECTOR3 posPlayer = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	if (pPlayer != nullptr) posPlayer = pPlayer->GetPos();
+
+	D3DXVECTOR3 posCamera = m_pos + (posPlayer - m_pos) / 2.0f;
+	posCamera.y = m_pos.y;
+
+	//------------------------------------
+	//視点と注視点の設定
+	//------------------------------------
+	m_posR = posCamera;
+	m_posV = D3DXVECTOR3(posCamera.x + sinf(m_rot.y) * m_fDist * cosf(m_rot.x + D3DX_PI),
+		posCamera.y + sinf(m_rot.x + D3DX_PI) * m_fDist,
+		posCamera.z + cosf(m_rot.y) * m_fDist * cosf(m_rot.x + D3DX_PI));	//視点が中心の場合から変更したときに移動がないようにX回転にPIを足している
+
+	//------------------------------------
+	//ビューマトリックスの作成
+	//------------------------------------
+	D3DXMatrixLookAtLH(&m_mtxView,
+		&m_posV,
+		&m_posR,
+		&m_vecU);
+	//ビューマトリックスの設定
+	pDevice->SetTransform(D3DTS_VIEW, &m_mtxView);	//ビルボードのために残す
+	//シェーダにビューマトリックスを設定
+	pRenderer->SetEffectMatrixView(m_mtxView);
+
+
+	//------------------------------------
+	//シェーダのライトの位置を更新
+	//------------------------------------
+	D3DXMATRIX mtxLightView;   // ライトビュー変換
+	D3DXVECTOR3 posLight;
+	//posLight = m_posV + D3DXVECTOR3(0.0f, 1600.0f, -800.0f);
+	posLight = D3DXVECTOR3(0.0f, 2200.0f, -1800.0f);
+	D3DXVECTOR3 vecLight = D3DXVECTOR3(0.0f, -1.0f, 0.4f);	//ライトのベクトル
+	//ライトのビューマトリックスを生成
+	D3DXMatrixLookAtLH(&mtxLightView, &posLight, &D3DXVECTOR3(posLight + vecLight), &D3DXVECTOR3(0, 1, 0));
+	pRenderer->SetEffectLightMatrixView(mtxLightView);
+
+}
+
+//=============================================================================
+// 移動
+//=============================================================================
+void CCamera::Move(void) {
+	CManager* pManager = CManager::GetManager();	//マネージャーの取得
+	CInput* pInput = nullptr;	//入力デバイスへのポインタ
+
+	//現在の入力デバイスの取得
+	if (pManager != nullptr) pInput = pManager->GetInputCur();
+	//nullの場合終了
+	if (pInput == nullptr) return;
+
+	//--------------------
+	//入力処理
+	//--------------------
+	float fMoveSpeed = 5.0f;
+	if (pInput->GetPress(CInput::CODE::DASH)) {
+		fMoveSpeed *= 3;
+	}
+
+	if (pInput->GetPress(CInput::CODE::MOVE_UP)) {
+		//前移動
+		m_pos.z += fMoveSpeed * cosf(m_rot.y);
+		m_pos.x += fMoveSpeed * sinf(m_rot.y);
+	}
+	if (pInput->GetPress(CInput::CODE::MOVE_DOWN)) {
+		//後ろ移動
+		m_pos.z -= fMoveSpeed * cosf(m_rot.y);
+		m_pos.x -= fMoveSpeed * sinf(m_rot.y);
+	}
+	if (pInput->GetPress(CInput::CODE::MOVE_LEFT)) {
+		//左移動
+		m_pos.x -= fMoveSpeed * cosf(m_rot.y);
+		m_pos.z += fMoveSpeed * sinf(m_rot.y);
+	}
+	if (pInput->GetPress(CInput::CODE::MOVE_RIGHT)) {
+		//右移動
+		m_pos.x += fMoveSpeed * cosf(m_rot.y);
+		m_pos.z -= fMoveSpeed * sinf(m_rot.y);
+	}
+	if (pInput->GetPress(CInput::CODE::MOVE_RISE)) {
+		//上昇
+		m_pos.y += fMoveSpeed;
+	}
+	if (pInput->GetPress(CInput::CODE::MOVE_FALL)) {
+		//下降
+		m_pos.y -= fMoveSpeed;
+	}
+}
+
+//=============================================================================
+// 回転
+//=============================================================================
+void CCamera::Rotate(void) {
+	CManager* pManager = CManager::GetManager();	//マネージャーの取得
+	CInput* pInput = nullptr;	//入力デバイスへのポインタ
+
+	//現在の入力デバイスの取得
+	if (pManager != nullptr) pInput = pManager->GetInputCur();
+	//nullの場合終了
+	if (pInput == nullptr) return;
+
+	//--------------------
+	//入力処理
+	//--------------------
+	if (pInput->GetPress(CInput::CODE::ROTATE_UP)) {
+		//視点を下げる
+		m_rot.x += CAMERA_ROTATE_SPEED_PITCH * D3DX_PI;
+		if (m_rot.x > MAX_ROTATE_PITCH * D3DX_PI) {
+			m_rot.x = MAX_ROTATE_PITCH * D3DX_PI;
+		}
+	}
+	if (pInput->GetPress(CInput::CODE::ROTATE_DOWN)) {
+		//視点を上げる
+		m_rot.x -= CAMERA_ROTATE_SPEED_PITCH * D3DX_PI;
+		if (m_rot.x < MIN_ROTATE_PITCH * D3DX_PI) {
+			m_rot.x = MIN_ROTATE_PITCH * D3DX_PI;
+		}
+	}
+
+	if (pInput->GetPress(CInput::CODE::ROTATE_LEFT)) {
+		//視点を左回転
+		m_rot.y -= CAMERA_ROTATE_SPEED_YAW * D3DX_PI;
+		if (m_rot.y < -D3DX_PI) {
+			m_rot.y = m_rot.y + D3DX_PI * 2;
+		}
+	}
+	if (pInput->GetPress(CInput::CODE::ROTATE_RIGHT)) {
+		//視点を右回転
+		m_rot.y += CAMERA_ROTATE_SPEED_YAW * D3DX_PI;
+		if (m_rot.y > D3DX_PI) {
+			m_rot.y = m_rot.y - D3DX_PI * 2;
+		}
+	}
+
+}
---
+++ 吉田学園情報ビジネス専門学校_鶴間俊樹/GlowApple/作品/プロジェクト/enemy.cpp
@@ -0,0 +1,507 @@
+//=============================================================================
+//
+// エネミー処理 [enemy.cpp]
+// Author : 鶴間俊樹
+//
+//=============================================================================
+#include "enemy.h"
+#include "manager.h"
+#include "sound.h"
+#include "gameScene.h"
+#include "player.h"
+#include "terrain.h"
+#include "effect.h"
+#include "billboard.h"
+#include "particleEffect.h"
+#include "gauge3D.h"
+#include "appleTree.h"
+#include "apple_black.h"
+
+//=============================================================================
+// マクロ定義
+//=============================================================================
+#define FALLDOWN_TIME (35)	//死亡時の倒れる時間
+#define FINISH_TIME_CHANGE_COLOR_GAMEOVER (60)	//死亡後の色変更にかかる時間
+#define START_CHANGE_COLOR_DELETE (180)	//死亡後の破棄開始時間
+#define FINISH_TIME_DELETE (60)		//死亡後の破棄にかかる時間
+
+#define GOLD_RATE (7)	//金の敵になる確率
+#define DEFAULT_GROW_VALUE (1)	//デフォルトの敵死亡時の林檎の木成長量
+#define GOLD_GROW_VALUE (10)		//金の敵死亡時の林檎の木成長量
+
+//=============================================================================
+// 静的メンバ変数宣言
+//=============================================================================
+bool CEnemy::m_bGoldRush = false;
+
+//=============================================================================
+// デフォルトコンストラクタ
+//=============================================================================
+CEnemy::CEnemy() : m_fMoveSpeed(0.0f), m_fRotateSpeed(0.0f), m_nMaxLife(0), m_fDistAttack(0.0f), m_fHeightLifeGauge(0.0f)
+{
+
+}
+
+//=============================================================================
+// オーバーロードされたコンストラクタ
+//=============================================================================
+CEnemy::CEnemy(const PARTS_INFO* pPartsInfoArray, int nNumParts, const MOTION_INFO* pMotionInfoArray, int nNumTypeMotion, float fMoveSpeed, float fRotSpeed, int nLife, float fDistAttack, float fHeightLifeGauge)
+	: CObjectMotion(pPartsInfoArray, nNumParts, pMotionInfoArray, nNumTypeMotion, false), m_fMoveSpeed(fMoveSpeed), m_fRotateSpeed(fRotSpeed), m_nMaxLife(nLife), m_fDistAttack(fDistAttack), m_fHeightLifeGauge(fHeightLifeGauge)
+{
+	SetUpdatePriority(UPDATE_PRIORITY::ENEMY);	//更新順の設定
+	SetDrawPriority(DRAW_PRIORITY::CHARA);	//描画順の設定
+	SetObjType(OBJTYPE_ENEMY);	//オブジェクトタイプの設定
+
+	SetColorOutlineAll(D3DXCOLOR(0.8f, 0.0f, 0.0f, 1.0f));	//輪郭の色の設定
+	SetColorGlowAll(D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f));	//輪郭の発光色の設定
+
+	m_move = D3DXVECTOR3(0.0f, 0.0f, 0.0f);
+	m_rotDestY = 0.0f;
+	m_nLife = nLife;
+	m_bDead = false;
+	m_nCntDead = 0;
+	m_nKillScore = 0;
+
+	m_bLand = false;
+	m_bAttackDist = false;
+	m_bGoldEnemy = false;
+
+	//体力ゲージの生成
+	m_pGaugeLife = CGauge3D::Create(m_nMaxLife, false, m_nLife, 600, false);
+	//ゲージの背景の設定
+	m_pGaugeLife->CreateGaugeBG(CTexture::TEXTURE_TYPE::NONE, D3DXVECTOR3(0.0f, 0.0f, 0.0f), 55.0f, 7.0f);
+	m_pGaugeLife->SetGaugeBGColor(D3DXCOLOR(0.3f, 0.3f, 0.3f, 1.0f));
+	//ゲージの設定
+	m_pGaugeLife->CreateGauge(CTexture::TEXTURE_TYPE::NONE, D3DXVECTOR3(0.0f, 0.0f, 0.0f), 50.0f, 4.0f);
+	m_pGaugeLife->SetGaugeColor(D3DXCOLOR(1.0f, 0.2f, 0.0f, 1.0f));
+	//ゲージを非表示
+	m_pGaugeLife->SetGaugeDraw(false);
+}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CEnemy::~CEnemy()
+{
+
+}
+
+//=============================================================================
+// エネミーの初期化処理
+//=============================================================================
+HRESULT CEnemy::Init(void) {
+	//当たり判定あり
+	SetEnableCollision(true);
+
+	//ランダムで金色の敵
+	int nRandGold = rand() % GOLD_RATE;
+	if (nRandGold == 0 || m_bGoldRush) {
+		m_bGoldEnemy = true;
+		SetDiffuseModelAll(D3DXCOLOR(0.7f, 0.5f, 0.0f, 1.0f), 0);
+		SetSpecularModelAll(D3DXCOLOR(0.8f, 0.8f, 0.8f, 1.0f), 0);
+	}
+
+	//モーションオブジェクトの初期化処理
+	CObjectMotion::Init();
+	return S_OK;
+}
+
+//=============================================================================
+// エネミーの終了処理
+//=============================================================================
+void CEnemy::Uninit(void) {
+	//体力ゲージの破棄
+	if (m_pGaugeLife != nullptr) {
+		m_pGaugeLife->Uninit();
+		m_pGaugeLife = nullptr;
+	}
+	//終了処理
+	CObjectMotion::Uninit();
+}
+
+//=============================================================================
+// エネミーの更新処理
+//=============================================================================
+void CEnemy::Update(void) {
+	//マネージャーの取得
+	CManager* pManager = CManager::GetManager();
+
+	//サウンドの取得
+	CSound *pSound = nullptr;
+	if (pManager != nullptr) pSound = pManager->GetSound();
+
+	//----------------------------
+	//死亡時
+	//----------------------------
+	if (m_bDead) {
+		m_nCntDead++;
+
+		//重力
+		SetPos(GetPos() + D3DXVECTOR3(0.0f, -POWER_GRAVITY_GROUND, 0.0f));
+
+		//地形との当たり判定
+		bool bLand = false;	//接地しているかどうか
+		D3DXVECTOR3 posColTerrain, vecStart, vecEnd;	//接触位置、開始ベクトル、終了ベクトル
+		vecStart = GetPos();	//敵の位置を取得
+		vecEnd = vecStart;
+		vecEnd.y += 1.0f;	//上向きのベクトル
+
+		bLand = CTerrain::Collision(&posColTerrain, vecStart, vecEnd);
+		//接地時
+		if (bLand) {
+			SetPos(posColTerrain);	//位置の移動
+		}
+
+		//倒れる時間
+		if (m_nCntDead == FALLDOWN_TIME) {
+			//倒れる音
+			if (pSound != nullptr)pSound->PlaySound(CSound::SOUND_LABEL::STUN);
+
+			//パーティクルエフェクトの生成
+			D3DXVECTOR3 posParticle;	//パーティクルの位置
+			GetPosCollision(&posParticle, D3DXVECTOR3(0.0f, 20.0f, 0.0f), 0);	//胴体の位置の取得
+
+			//パーティクルエフェクトの最初の色
+			D3DXCOLOR colEffectStart = D3DXCOLOR(0.0f, 0.0f, 1.0f, 1.0f);
+			if (m_bGoldEnemy) colEffectStart = D3DXCOLOR(1.0f, 1.0f, 0.0f, 1.0f);
+
+			//パーティクルの情報
+			CParticleEffect::PARTICLE_INFO particleInfo = { 60, 25.0f, -0.25f, 2.0f, D3DXVECTOR3(0.0f, -0.01f, 0.0f), colEffectStart, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f) };
+			//エフェクトの生成
+			CParticleEffect::Create(particleInfo, posParticle, START_CHANGE_COLOR_DELETE - FALLDOWN_TIME, 10, 0.05f * D3DX_PI, true);
+
+			//色を白く変更
+			StartChangeDiffuseAll(0, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f), FINISH_TIME_CHANGE_COLOR_GAMEOVER);
+		}
+
+		//エネミーの色の変更開始
+		if (m_nCntDead == START_CHANGE_COLOR_DELETE) {
+			//透明にしていく
+			StartChangeDiffuseAll(0, D3DXCOLOR(1.0f, 1.0f, 1.0f, 0.0f), FINISH_TIME_DELETE);
+		}
+
+		//エネミーの破棄
+		if (m_nCntDead >= START_CHANGE_COLOR_DELETE + FINISH_TIME_DELETE) {
+			//終了処理
+			Uninit();
+			return;
+		}
+
+		//モーションの更新
+		CObjectMotion::Update();
+		return;
+	}
+
+
+	//----------------------------
+	//回転
+	//----------------------------
+	D3DXVECTOR3 rot = GetRot();	//角度の取得
+	
+	//目標角度と角度が不一致の場合
+	if (m_rotDestY != GetRot().y) {	//GetMotionCategory() != MOTION_CATEGORY::ATTACK && 
+
+		//目標角度へ回転させる
+		float rotY = rot.y;	//Y軸の角度
+
+		//目標角度がマイナス、角度がプラスのときに、減算した結果が-πを超えた場合
+		if (m_rotDestY - rotY < -D3DX_PI) {
+			//加算
+			rot.y += m_fRotateSpeed;
+		}
+		//目標角度がプラス、角度がマイナスのときに、減算した結果がπを超えた場合
+		else if (m_rotDestY - rotY > D3DX_PI) {
+			//減算
+			rot.y -= m_fRotateSpeed;
+		}
+		//個々の正負は関係ない場合
+		else if (m_rotDestY - rotY > 0) {
+			//加算
+			rot.y += m_fRotateSpeed;
+			//目標角度超過時の調整
+			if (rot.y > m_rotDestY) {
+				rot.y = m_rotDestY;
+			}
+		}
+		else if (m_rotDestY - rotY < 0) {
+			//減算
+			rot.y -= m_fRotateSpeed;
+			//目標角度超過時の調整
+			if (rot.y < m_rotDestY) {
+				rot.y = m_rotDestY;
+			}
+		}
+
+
+		//π超過時の調整
+		if (rot.y > D3DX_PI) {
+			rot.y -= D3DX_PI * 2;
+		}
+		else if (rot.y < -D3DX_PI) {
+			rot.y += D3DX_PI * 2;
+		}
+
+		//角度の設定
+		SetRot(rot);
+	}
+
+	//----------------------------
+	//移動
+	//----------------------------
+	//移動
+	if (m_bMove) {
+		m_move.x = sinf(rot.y + D3DX_PI) * m_fMoveSpeed;
+		m_move.z = cosf(rot.y + D3DX_PI) * m_fMoveSpeed;
+	}
+
+	//位置の設定
+	SetPos(GetPos() + m_move);
+
+	//攻撃可能な距離かどうか計算
+	AttackDistance();
+
+	//----------------------------
+	//当たり判定
+	//----------------------------
+	//地面との当たり判定
+	bool bLand = false;	//接地しているかどうか
+	D3DXVECTOR3 posColTerrain, vecStart, vecEnd;	//接触位置、開始ベクトル、終了ベクトル
+	vecStart = GetPos();	//敵の位置を取得
+	vecEnd = vecStart;
+	vecEnd.y += 1.0f;	//上向きのベクトル
+
+	bLand = CTerrain::Collision(&posColTerrain, vecStart, vecEnd);
+	//接地時
+	if (bLand) {
+		m_bLand = true;
+		m_move.y = -POWER_GRAVITY_GROUND;
+		SetPos(posColTerrain);	//位置の移動
+
+		//木の方へ目標角度を設定
+		SetRotTree();
+
+		//魔術師敵が移動モーションなしにしているので移動不可時に接地していれば移動モーション
+		if (!m_bMove && GetMotionCategory() != MOTION_CATEGORY::ATTACK) {
+			//移動モーションの設定
+			SetMoveMotion();
+			m_bMove = true;
+		}
+	}
+	else {
+		m_bLand = false;
+		//重力を掛ける
+		m_move.y -= POWER_GRAVITY;
+	}
+
+	//落下死
+	if (GetPos().y < DEAD_HEIGHT) {
+		Uninit();
+		return;
+	}
+
+	//攻撃
+	Attack();
+
+	//モーションの更新
+	CObjectMotion::Update();
+
+	//モーション終了時
+	if (GetEndMotion()) {
+		MotionEnd();
+	}
+
+	//オブジェクトの全モデルのワールドマトリックスを更新
+	UpdateMtxWorldAll();
+	//モーション中の処理
+	MotionAct();
+
+	if (m_pGaugeLife != nullptr) {
+		//体力ゲージの位置を更新
+		m_pGaugeLife->SetAllGaugePos(D3DXVECTOR3(GetPos().x, GetPos().y + m_fHeightLifeGauge, GetPos().z));
+	}
+}
+
+//=============================================================================
+// エネミーの描画処理
+//=============================================================================
+void CEnemy::Draw(void) {
+	CObjectMotion::Draw();
+}
+
+//=============================================================================
+// 移動量の設定
+//=============================================================================
+void CEnemy::SetMove(D3DXVECTOR3 move) {
+	m_move = move;
+	//弾き飛ばされたりしてジャンプするような事があればニュートラルモーションを設定（移動モーション中は移動を固定されるため）
+	if (m_move.y > 0.0f) {
+		SetMotion(0);
+		AttackStop();
+		m_bMove = false;
+	}
+}
+
+//=============================================================================
+// エネミーのダメージ
+//=============================================================================
+void CEnemy::Damage(int nDamage, DAMAGE_TYPE typeDamage, bool* pDead) {
+	if (m_bDead) return;
+
+	//ダメージ
+	m_nLife -= nDamage;
+
+	//マネージャーの取得
+	CManager* pManager = CManager::GetManager();
+	//サウンドの取得
+	CSound *pSound = nullptr;
+	if (pManager != nullptr) pSound = pManager->GetSound();
+
+	//攻撃のタイプごとの処理
+	switch (typeDamage)
+	{
+	case DAMAGE_TYPE::PLAYER_PUNCH:
+	case DAMAGE_TYPE::SHOCKWAVE:
+		//攻撃エフェクトの生成
+		CEffect::Create(GetPos() + D3DXVECTOR3(0.0f, 50.0f, 0.0f), CEffect::EFFECT_TYPE::DAMAGE_PLAYER, 40.0f, 40.0f, false);
+		//ダメージ音の再生
+		if (pSound != nullptr) pSound->CSound::PlaySound(CSound::SOUND_LABEL::DAMAGE_PUNCH);
+		break;
+	case DAMAGE_TYPE::THUNDERBOLT:
+		//ダメージエフェクトの生成
+		CEffect::Create(GetPos() + D3DXVECTOR3(0.0f, 50.0f, 0.0f), CEffect::EFFECT_TYPE::THUNDER, 50.0f, 50.0f, false);
+		//ダメージ音の再生
+		if (pSound != nullptr) pSound->CSound::PlaySound(CSound::SOUND_LABEL::DAMAGE_THUNDER);
+		break;
+	}
+
+	//死亡判定
+	if (m_nLife <= 0) {
+		//死亡
+		if (pDead != nullptr) *pDead = true;
+		Dead();
+	}
+	else {
+		if (pDead != nullptr) *pDead = false;
+
+		//体力ゲージの設定
+		if (m_pGaugeLife != nullptr) {
+			m_pGaugeLife->SetGaugeValue(m_nLife);
+		}
+	}
+}
+
+//=============================================================================
+// エネミーの死亡
+//=============================================================================
+void CEnemy::Dead(void) {
+	//死亡の設定
+	m_bDead = true;
+	//死亡カウントの初期化
+	m_nCntDead = 0;
+	//当たり判定を無効
+	SetEnableCollision(false);
+	//死亡モーションの設定
+	SetDeadMotion();
+	//輪郭を非表示
+	SetDrawOutlineAll(false);
+
+	//体力ゲージの破棄
+	if (m_pGaugeLife != nullptr) {
+		m_pGaugeLife->Uninit();
+		m_pGaugeLife = nullptr;
+	}
+
+	//マネージャーの取得
+	CManager* pManager = CManager::GetManager();
+
+	//ゲームシーンの取得
+	CGameScene* pGame = nullptr;
+	if (pManager != nullptr) pGame = pManager->GetGameScene();
+	//林檎の木の取得
+	CAppleTree* pAppleTree = nullptr;
+	if (pGame != nullptr) {
+		//倒したスコアの加算
+		pGame->AddGameScore(m_nKillScore);
+		//リンゴの木を取得
+		pAppleTree = pGame->GetAppleTree();
+	}
+
+	//林檎の木を成長させる
+	if (pAppleTree != nullptr) {
+		int nGrowValue = DEFAULT_GROW_VALUE; //成長量
+		//金の敵の場合多く成長
+		if (m_bGoldEnemy) nGrowValue = GOLD_GROW_VALUE;
+		//木を成長
+		pAppleTree->AddGrow(nGrowValue);
+	}
+
+	//サウンドの取得
+	CSound* pSound = nullptr;
+	if (pManager != nullptr) pSound = pManager->GetSound();
+	//音の再生
+	//if (pSound != nullptr) pSound->PlaySound(CSound::SOUND_LABEL::EXPLOSION);
+}
+
+//=============================================================================
+// 木の方向への角度に設定
+//=============================================================================
+void CEnemy::SetRotTree(void) {
+	//マネージャーの取得
+	CManager* pManager = CManager::GetManager();
+	if (pManager == nullptr) return;
+	//ゲームシーンの取得
+	CGameScene* pGame = pManager->GetGameScene();
+	if (pGame == nullptr) return;
+	//林檎の木の取得
+	CObject* pAppleTree = pGame->GetAppleTree();
+	if (pAppleTree == nullptr) return;
+	D3DXVECTOR3 posAppleTree = pAppleTree->GetPos();//木の位置を取得
+
+	//敵から林檎の木への角度
+	float rotTreeY = (float)atan2(posAppleTree.x - GetPos().x, posAppleTree.z - GetPos().z);
+
+	//目標角度を林檎の木の向きへ向ける
+	m_rotDestY = rotTreeY;
+	//モデルが反対なので反対向きに向ける
+	if (m_rotDestY >= 0.0f) {
+		m_rotDestY -= D3DX_PI;
+	}
+	else if (m_rotDestY < 0.0f) {
+		m_rotDestY += D3DX_PI;
+	}
+}
+
+//=============================================================================
+// 攻撃可能な距離かどうかを計算
+//=============================================================================
+void CEnemy::AttackDistance(void) {
+	//マネージャーの取得
+	CManager* pManager = CManager::GetManager();
+	if (pManager == nullptr) return;
+	//ゲームシーンの取得
+	CGameScene* pGame = pManager->GetGameScene();
+	if (pGame == nullptr) return;
+	//林檎の木の取得
+	CObject* pAppleTree = pGame->GetAppleTree();
+	if (pAppleTree == nullptr) return;
+	D3DXVECTOR3 posAppleTree = pAppleTree->GetPos();//木の位置を取得
+	float fDistTree = D3DXVec2Length(&D3DXVECTOR2(posAppleTree.x - GetPos().x, posAppleTree.z - GetPos().z));	//木と敵との距離
+
+	m_bAttackDist = fDistTree <= m_fDistAttack ? true : false;
+}
+
+//=============================================================================
+//　攻撃の処理
+//=============================================================================
+void CEnemy::Attack(void) {
+	//攻撃開始条件を満たしていない場合終了
+	if (GetMotionCategory() == MOTION_CATEGORY::ATTACK || !m_bAttackDist || !m_bLand) return;
+
+	//攻撃済みリストの初期化
+	InitObjAttacked();
+	//移動量を重力のみにする
+	m_move = D3DXVECTOR3(0.0f, -POWER_GRAVITY_GROUND, 0.0f);
+	m_bMove = false;
+	//攻撃開始
+	AttackStart();
+}
